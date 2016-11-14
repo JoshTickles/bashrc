d141ebf865c40fa2939be190db1f3f84658aa3a2
JoshTickles <joshangel@hotmail.co.uk>
2016-11-14 15:22:18 +1300

    Create bashrc-config.sh

---
+++ bashrc-config.sh
@@ -0,0 +1,119 @@
+#!/bin/bash/sh
+#
+# Requires Ubuntu distribution - This should work fine on versions 16.04 and below...	
+# 
+#
+currentver="0.62"
+currentverdate="1sh July 2016"
+
+WhichDistAmI()
+{
+	# Check for Ubuntu - This should work fine on versions 16.04 and below...	
+	if [ -f "/usr/bin/lsb_release" ];
+	then
+		ubuntuVersion=`lsb_release -s -d`
+
+		case $ubuntuVersion in
+			*"Ubuntu"*)
+				OS="Ubuntu"
+				export OS
+			;;
+
+			*)
+				echo -e "Script is for Ubuntu OS only. Exiting."
+				exit 1
+			;;
+		esac
+	fi
+}
+
+AmIroot()
+{
+	# Check for root, quit if not present with a warning.
+	if [ "$(id -u)" != "0" ];
+	then
+		echo "\nScript needs to be run as root. Please elevate and run again!"
+		exit 1
+	else
+		echo "\nScript running as root. Starting..."
+	fi
+
+#---- .bashrc adds
+
+#-------------------------------------------------------------
+# Greeting, motd etc. ...
+#-------------------------------------------------------------
+
+# Color definitions (taken from Color Bash Prompt HowTo).
+# Some colors might look different of some terminals.
+# For example, I see 'Bold Red' as 'orange' on my screen,
+# hence the 'Green' 'BRed' 'Red' sequence I often use in my prompt.
+
+# Normal Colors
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+
+# Bold
+BBlack='\e[1;30m'       # Black
+BRed='\e[1;31m'         # Red
+BGreen='\e[1;32m'       # Green
+BYellow='\e[1;33m'      # Yellow
+BBlue='\e[1;34m'        # Blue
+BPurple='\e[1;35m'      # Purple
+BCyan='\e[1;36m'        # Cyan
+BWhite='\e[1;37m'       # White
+
+# Background
+On_Black='\e[40m'       # Black
+On_Red='\e[41m'         # Red
+On_Green='\e[42m'       # Green
+On_Yellow='\e[43m'      # Yellow
+On_Blue='\e[44m'        # Blue
+On_Purple='\e[45m'      # Purple
+On_Cyan='\e[46m'        # Cyan
+On_White='\e[47m'       # White
+
+NC="\e[m"               # Color Reset
+
+#--------------------ALIASES
+alias ls='ls -h --color' # ls colour enable
+ 
+#Spelling error correction
+alias xs='cd'
+alias vf='cd'
+alias moer='more'
+alias moew='more'
+alias kk='ll'
+#--------------------FUNCTIONS
+#
+#--------------------
+# dotdate func. Copies a file and appends date - ideal for backups
+# Use: dotdate file.txt 
+# Result: file.txt.2016.11.14-13.40
+function dotdate () {
+	cp -r $1 $1.$(date +%Y-%m-%d-%H%M )
+	}
+#-------------------
+# 
+function ii()   # Get current host related info.
+{
+    echo -e "\nYou are logged on ${BRed}$HOST"
+    echo -e "\n${BRed}Additionnal information:$NC " ; uname -a
+    echo -e "\n${BRed}Users logged on:$NC " ; w -hs |
+             cut -d " " -f1 | sort | uniq
+    echo -e "\n${BRed}Current date :$NC " ; date
+    echo -e "\n${BRed}Machine stats :$NC " ; uptime
+    echo -e "\n${BRed}Memory stats :$NC " ; free
+    echo -e "\n${BRed}Diskspace :$NC " ; mydf / $HOME
+    echo -e "\n${BRed}Local IP Address :$NC" ; my_ip
+    echo -e "\n${BRed}Open connections :$NC "; netstat -pan --inet;
+    echo
+}
+#-------------------
+

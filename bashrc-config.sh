9baae95e95a2b23220e4f3e6d49e59cb2b392417
JoshTickles <joshangel@hotmail.co.uk>
2016-11-22 09:12:13 +1300

    Update bashrc-config.sh

--- bashrc-config.sh
+++ bashrc-config.sh
@@ -37,157 +37,22 @@ AmIroot()
 	else
 		echo "\nScript running as root. Starting..."
 	fi
-
-#---- .bashrc adds
-
-#-------------------------------------------------------------
-# Greeting, motd etc. ...
-#-------------------------------------------------------------
-
-# Color definitions (taken from Color Bash Prompt HowTo).
-# Some colors might look different of some terminals.
-# For example, I see 'Bold Red' as 'orange' on my screen,
-# hence the 'Green' 'BRed' 'Red' sequence I often use in my prompt.
-
-# Normal Colors
-Black='\e[0;30m'        # Black
-Red='\e[0;31m'          # Red
-Green='\e[0;32m'        # Green
-Yellow='\e[0;33m'       # Yellow
-Blue='\e[0;34m'         # Blue
-Purple='\e[0;35m'       # Purple
-Cyan='\e[0;36m'         # Cyan
-White='\e[0;37m'        # White
-
-# Bold
-BBlack='\e[1;30m'       # Black
-BRed='\e[1;31m'         # Red
-BGreen='\e[1;32m'       # Green
-BYellow='\e[1;33m'      # Yellow
-BBlue='\e[1;34m'        # Blue
-BPurple='\e[1;35m'      # Purple
-BCyan='\e[1;36m'        # Cyan
-BWhite='\e[1;37m'       # White
-
-# Background
-On_Black='\e[40m'       # Black
-On_Red='\e[41m'         # Red
-On_Green='\e[42m'       # Green
-On_Yellow='\e[43m'      # Yellow
-On_Blue='\e[44m'        # Blue
-On_Purple='\e[45m'      # Purple
-On_Cyan='\e[46m'        # Cyan
-On_White='\e[47m'       # White
-
-NC="\e[m"               # Color Reset
-
-#--------------------ALIASES
-alias ls='ls -h --color' # ls colour enable
-alias grep='grep --color=auto' # grep colour enable
-alias dfh='df -h' # human readable disk space
-alias back='cd $OLDPWD' # Lazy way back to previous pwd (like cd -) 
-alias update="sudo apt-get update"
-alias shutdown="sudo shutdown -h now"
-alias upgrade="sudo apt-get upgrade"
-alias snano="sudo nano"
-alias root="sudo su"
-alias purge="sudo apt-get remove --purge"
-alias clean="sudo apt-get autoclean"
-# Spelling / typo error correction
-alias xs='cd'
-alias vf='cd'
-alias moer='more'
-alias moew='more'
-alias kk='ll'
-alias ..='cd ..'
-alias cd..='cd ..'
-#--------------------OTHER
-# Clock - A bash clock that can run in your terminal window.
-clock (){
-while true;do clear;echo "===========";date +"%r";echo "===========";sleep 1;done
-}
-# cp with progress bar
-function cp_p() {
-	if [ `echo "$2" | grep ".*\/$"` ]
-	then
-		pv "$1" > "$2""$1"
-	else
-		pv "$1" > "$2"/"$1"
-	fi
-}
-#--------------------FUNCTIONS
-#
-#--------------------
-# dotdate func. Copies a file and appends date - ideal for backups
-# Use: dotdate file.txt 
-# Result: file.txt.2016.11.14-13.40
-function dotdate () {
-	cp -r $1 $1.$(date +%Y-%m-%d-%H%M )
 	}
-#-------------------
-function mydf()         # Pretty-print of 'df' output.
-{                       # Inspired by 'dfc' utility.
-    for fs ; do
 
-        if [ ! -d $fs ]
-        then
-          echo -e $fs" :No such file or directory" ; continue
-        fi
 
-        local info=( $(command df -P $fs | awk 'END{ print $2,$3,$5 }') )
-        local free=( $(command df -Pkh $fs | awk 'END{ print $4 }') )
-        local nbstars=$(( 20 * ${info[1]} / ${info[0]} ))
-        local out="["
-        for ((j=0;j<20;j++)); do
-            if [ ${j} -lt ${nbstars} ]; then
-               out=$out"*"
-            else
-               out=$out"-"
-            fi
-        done
-        out=${info[2]}" "$out"] ("$free" free on "$fs")"
-        echo -e $out
-    done
-}
-#-------------------
-function my_ip() # Get IP adress on ethernet.
-{
-    MY_IP=$(/sbin/ifconfig eth0 | awk '/inet/ { print $2 } ' |
-      sed -e s/addr://)
-    echo ${MY_IP:-"Not connected"}
-}
-#------------------- 
-function ii()   # Get current host related info.
-{
-    echo -e "\nYou are logged on ${BRed}" ; hostname
-    echo -e "\n${BRed}Additionnal information:$NC " ; uname -a
-    echo -e "\n${BRed}Users logged on:$NC " ; w -hs |
-             cut -d " " -f1 | sort | uniq
-    echo -e "\n${BRed}Current date :$NC " ; date
-    echo -e "\n${BRed}Machine stats :$NC " ; uptime
-    echo -e "\n${BRed}Memory stats :$NC " ; free
-    echo -e "\n${BRed}Diskspace :$NC " ; mydf / $HOME
-    echo -e "\n${BRed}Local IP Address(es) :$NC" ; hostname -I
-    echo -e "\n${BRed}Open connections :$NC "; netstat -pan --inet;
-    echo
-}
-#-------------------
-function extract () { # Easy archive management.
-	if [ -f $1 ] ; then
-		case $1 in
-			*.tar.bz2)	tar xjf $1		;;
-			*.tar.gz)	tar xzf $1		;;
-			*.bz2)		bunzip2 $1		;;
-			*.rar)		rar x $1		;;
-			*.gz)		gunzip $1		;;
-			*.tar)		tar xf $1		;;
-			*.tbz2)		tar xjf $1		;;
-			*.tgz)		tar xzf $1		;;
-			*.zip)		unzip $1		;;
-			*.Z)		uncompress $1	;;
-			*)			echo "'$1' cannot be extracted via extract()" ;;
-		esac
-	else
-		echo "'$1' is not a valid file"
-	fi
-}
+WhichUser()
+{ 
+	# Ask which user .bashrc to append
+ 	echo "Please enter your account username: "
+		read Username
+	echo "You entered: $Username"
+	sleep 3
+	echo "Applying configuration to $Username's .bashrc..."
+ 	sleep 3
+ 		save="/home/$Username/.bashrc"
+ 	echo "" >> $save #Create New line in .bash_aliases
+ 	echo ./bashrc.txt >> $save
+	sleep 3
+ 	echo "Configuration has been added... You must exit your session for these to take effect."
+	
+	}
